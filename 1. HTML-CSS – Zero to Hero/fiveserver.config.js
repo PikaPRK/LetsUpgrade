ebf15a108f09301e387c4b7861c278b49552a5f3
Pratap Kumar Bal <84920285+PikaPRK@users.noreply.github.com>
2021-07-07 11:46:09 +0530

    Delete 1. HTML-CSS – Zero to Hero directory

--- 1. HTML-CSS – Zero to Hero/fiveserver.config.js
+++
@@ -1,9 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-    highlight: true,
-    injectBody: true,
-    remoteLogs: true,
-    remoteLogs: "yellow",
-    injectCss: true,
-    navigate: true,
-  };
