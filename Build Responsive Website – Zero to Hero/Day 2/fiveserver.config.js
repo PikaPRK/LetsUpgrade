944c973d3c0d4b2c9b4e2835c736fd457327f79b
Pratap Kumar Bal <84920285+PikaPRK@users.noreply.github.com>
2021-08-01 09:14:34 +0530

    Add files via upload

---
+++ Build Responsive Website – Zero to Hero/Day 2/fiveserver.config.js
@@ -0,0 +1,9 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: true,
+    injectBody: true,
+    remoteLogs: true,
+    remoteLogs: "yellow",
+    injectCss: true,
+    navigate: true,
+  };
